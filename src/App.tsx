4d3f459704a2e57cb59125a0c28e4cb3f106ccb0
Dennis Augusto <poxaugustodantas@gmail.com>
2025-10-01 10:38:01 -0300

    routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+export default function App() {
   return (
-    <>
-      <div className='mx-auto bg-amber-300'>hello world</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className='mx-auto bg-amber-300'>home</div>
+    </>
+  );
+}
